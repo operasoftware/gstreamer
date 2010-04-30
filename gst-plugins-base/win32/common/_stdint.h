729fbbad04d025008f9e2ea1d3c2c35a0329d50a
Philip Jägenstedt <philipj@opera.com>
2010-04-30 10:47:12 +0200

    customize win32/common/_stdint.h

--- gst-plugins-base/win32/common/_stdint.h
+++ gst-plugins-base/win32/common/_stdint.h
@@ -1,9 +1,7 @@
 #ifndef _GST_PLUGINS_BASE__STDINT_H
 #define _GST_PLUGINS_BASE__STDINT_H 1
-#ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "gst-plugins-base 0.10.29"
-/* generated using gnu compiler gcc (Debian 4.4.3-9) 4.4.3 */
-#define _STDINT_HAVE_STDINT_H 1
-#include <stdint.h>
-#endif
+#include <glib.h>
+typedef guint8 uint8_t;
+typedef guint16 uint16_t;
+typedef guint32 uint32_t;
 #endif
